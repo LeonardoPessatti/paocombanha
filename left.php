b40b1346fc56bea562fc506e1181eeae6daac002
Unknown <lpessatti@gmail.com>
2017-07-15 19:39:45 -0300

    Inicio construção coluna left

--- left.php
+++ left.php
@@ -1,8 +1,17 @@
+  <div class="row">
+            <div class="card large col s10 m8 l2 offset-l10 offset-s1 offset-m2" style="height: 35em; padding-bottom: 1.5em">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/placeholder.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+    </div>
+  </div>
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
