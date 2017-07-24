1ad16720f30db94f1fd4b820887560c9f1f5d5e7
Unknown <lpessatti@gmail.com>
2017-07-23 23:43:55 -0300

    Inclusão iframe Spotify

--- left.php
+++ left.php
@@ -1,17 +1,17 @@
     <div class="card large s10 m8 l2" style="height: 35em; padding-bottom: 1.5em">
-        <div class="card-image waves-effect waves-block waves-light">
+<!--        <div class="card-image waves-effect waves-block waves-light">
             <img class="activator" src="https://i.ytimg.com/vi/W7xYZwEFGc8/maxresdefault.jpg">
-        </div>
+        </div>-->
         <div class="card-content">
-            <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-            <p><a href="#">This is a link</a></p>
+            <iframe src="https://open.spotify.com/embed/user/22tu3lkw2zhowruezy2clbtra/playlist/6YJJmoa3TwgnlMvjAAkAun" width="400" height="470" frameborder="0" allowtransparency="true"></iframe>
         </div>
-        <div class="card-reveal">
+<!--        <div class="card-reveal">
             <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
             <p>Here is some more information about this product that is only revealed once clicked on.</p>
-        </div>
+        </div>-->
     </div>
 <!--<div id="bitvalor-widget"></div>
 <script type="text/javascript" src="//bitvalor.com/js/widget.min.js"></script>-->
+
 <?php
 
