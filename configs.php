191ea74efce6385453fe86eed15dbb75962f04d4
Unknown <lpessatti@gmail.com>
2017-08-06 01:15:04 -0300

    Classes incabadas

---
+++ configs.php
@@ -0,0 +1,62 @@
+<?php
+/* 
+ * Código criado e pertencente exclusivamente a Leonardo Pessatti.
+ * www.facebook.com/leopessatti
+ */
+?>
+<head>
+  <meta charset="utf-8">
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+</head>
+<div class="container">
+                <div class="row center">
+                        <form  id="formMail" name="formMail" method="post" > action="http://www.jsdb.com.br"
+                                <h1>Entre em contato</h1>
+                                                        <input placeholder="Nome do site" name="nome" id="nome" type="text" class="validate" required="required">
+                                                        <input placeholder="E-mail do adminsitrador" name="emailadm" id="emailadm" type="email" class="validate" required="required">
+                                                        <input placeholder="E-mail para contato" name="email" id="email" type="email" class="validate" required="required">
+                                                        <textarea placeholder="Sua mensagem" name="mensagem" id="mensagem" class="materialize-textarea" required="required"></textarea>
+                                                        <button class="btn waves-effect waves-light" style=" background-color: #00adef !important" type="submit" name="submit">Enviar</button>
+                  </form>
+                </div>
+        </div>
+
+<?php
+error_reporting(E_ALL & ~E_NOTICE ^ E_WARNING);
+//date_default_timezone_set('America/Brasilia');
+//echo date_default_timezone_set('America/Brasilia');
+//$para = 'contato@jsdb.com.br';
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$msg = $_POST['mensagem'];
+$assunto = 'Contato pelo site ' . $email;
+$submit == $_POST['submit'];
+$conteudo = 'Enviado por ' . $nome . ', email: ' . $email . ', รกs ' . date("H:i:s") . '<br><br>' . $msg;
+
+$headers = "Content-Type:text/html; charset=UTF-8";
+$headers .= "From:  Contato Site JSDB<lpessatti@gmail.com.br>";
+$headers .= "X-Sender:  <suporte@jsdb.com.br>";
+$headers .= "X-Mailer: PHP  v" . phpversion();
+$headers .= "X-IP:  " . $_SERVER['REMOTE_ADDR'];
+$headers .= "Return-Path:  <" . $email . ">";
+
+
+$patterns = array();
+$patterns[0] = '\"';
+$patterns[1] = '\'';
+$patterns[2] = ';';
+$replacements = array();
+$replacements[0] = '\\"';
+$replacements[1] = "\'";
+$replacements[2] = '\;';
+preg_replace($patterns, $replacements, $msg);
+
+
+if (!empty($email)) {
+    mail($para, $assunto, $conteudo, $headers);
+    unset($email);
+}
+?>
--- left.php
+++ left.php
@@ -1,9 +1,12 @@
-    <div class="card large s10 m8 l2" style="height: 35em; padding-bottom: 1.5em">
+ <style type="text/css">
+    iframe {display:block; width:100%; border:none}
+  </style>
+  <div class="card large s0 m8 l2 left" style="padding:0;" id="left">
 <!--        <div class="card-image waves-effect waves-block waves-light">
             <img class="activator" src="https://i.ytimg.com/vi/W7xYZwEFGc8/maxresdefault.jpg">
         </div>-->
-        <div class="card-content">
-            <iframe src="https://open.spotify.com/embed/user/22tu3lkw2zhowruezy2clbtra/playlist/6YJJmoa3TwgnlMvjAAkAun" width="400" height="470" frameborder="0" allowtransparency="true"></iframe>
+        <div name="iframe1" style="padding:0; margin:0; width: auto">
+            <iframe src="https://open.spotify.com/embed/user/22tu3lkw2zhowruezy2clbtra/playlist/6YJJmoa3TwgnlMvjAAkAun"  height="500" frameborder="0" allowtransparency="true"></iframe>
         </div>
 <!--        <div class="card-reveal">
             <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
